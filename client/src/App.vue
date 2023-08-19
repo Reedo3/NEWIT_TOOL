0f111975926442d4849e800d8afcfa441216523c
Reedom <reedomrahman@gmail.com>
2023-08-19 00:02:53 -0400

    Vue project has basic search bar template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <search-bar @search="performSearch"></search-bar>
+    <search-results :results="searchResults"></search-results>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import SearchBar from './components/SearchBar.vue';
+import SearchResults from './components/SearchResults.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    SearchResults
+  },
+  setup() {
+    const searchResults = ref([]);
+
+//    function performSearch(query) {
+  function performSearch() {
+      // Simulate search results (replace with actual search logic)
+      searchResults.value = ['Result 1', 'Result 2', 'Result 3'];
+    }
+
+    return {
+      searchResults,
+      performSearch
+    };
   }
-}
+};
 </script>
 
 <style>
---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="search-bar">
+    <input
+      v-model="searchQuery"
+      @input="handleInput"
+      placeholder="Search..."
+    />
+    <button @click="search">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: ''
+    };
+  },
+  methods: {
+    handleInput() {
+      // Perform real-time filtering or validation here
+    },
+    search() {
+      this.$emit('search', this.searchQuery);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.search-bar {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+input {
+  padding: 5px;
+  border: 1px solid #ccc;
+}
+
+button {
+  margin-left: 5px;
+}
+</style>
